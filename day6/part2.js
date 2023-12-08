ba445f003e4b24857fb48efc997920cd3cf46d36
Drew <drewalexander986@gmail.com>
2023-12-08 15:54:49 +0800

    day 6

---
+++ day6/part2.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs');
+
+const [timeText, distanceText] = fs.readFileSync('./input.txt').toString().replaceAll(' ', '').split('\n').filter(text => text !== '')
+const matchedTime = timeText.match(/\d+/g)
+const matchedDistance = distanceText.match(/\d+/g) 
+const records = matchedTime.map((time, i) => ({ time: Number(time), distance: Number(matchedDistance[i])}))
+
+const raceOptions = records.map(race => {
+    let winOptions = 0
+    let holdTime = 0
+    while(holdTime < race.time) {
+        if (getRaceDistance(holdTime, race.time) > race.distance) winOptions++
+        holdTime++
+    }
+    return winOptions
+})
+
+function getRaceDistance(holdTime, raceLength) {
+    return (raceLength - holdTime) * holdTime 
+}
+
+const marginOfError = raceOptions.reduce((acc, curr) => acc * curr)
+
+console.log(marginOfError)
